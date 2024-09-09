f8021fe31cb38c1fc837487bd0e53f4549f90544
Edward Stables <edward.stables1198@gmail.com>
2024-09-09 20:11:56 +0100

    Initial commit with basic grpc implementation
    Pretty much copied the helloworld example, but put own proto in and
    testing that the cmake file works

---
+++ simulator.cpp
@@ -0,0 +1,41 @@
+#include <grpcpp/server_builder.h>
+#include <iostream>
+#include <string>
+#include <string_view>
+#include <memory>
+
+#include "sim_control.grpc.pb.h"
+
+using grpc::Channel;
+using grpc::ServerContext;
+using grpc::Status;
+using grpc::ServerBuilder;
+using grpc::Server;
+
+class SimulatorServerImpl final : public SimInspector::Service {
+    Status SendCommand(
+            ServerContext* context,
+            const Command* command,
+            CommandResponse* resp
+    ) override {
+        std::cout << "Got command in server" << std::endl;
+        resp->set_ok(true);
+        return Status::OK;
+    }
+};
+
+void run_server() {
+    std::string server_address("0.0.0.0:50101");
+    SimulatorServerImpl service{};
+
+    ServerBuilder builder;
+    builder.AddListeningPort(server_address, grpc::InsecureServerCredentials());
+    builder.RegisterService(&service);
+    std::unique_ptr<Server> server(builder.BuildAndStart());
+    std::cout << "Server listening on " << server_address << std::endl;
+    server->Wait();
+}
+
+int main() {
+    run_server();
+}
