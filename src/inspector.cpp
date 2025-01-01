c756c054e43dbda64bad3940dabdf27739e8e092
Edward Stables <edward.stables1198@gmail.com>
2025-01-01 13:30:06 +0000

    add very basic program rendering to inspector

--- src/inspector.cpp
+++ src/inspector.cpp
@@ -4,6 +4,7 @@
 #include "extensions/olcPGEX_QuickGUI.h"
 #include "../defs/defs_pkg.h"
 
+const int SOURCE_VIEW_WIDTH = 160;
 const int TOOLBAR_HEIGHT = 32;
 const int FRAMEBUFFER_LEN = vpu::defs::FRAMEBUFFER_WIDTH * vpu::defs::FRAMEBUFFER_HEIGHT;
 
@@ -12,7 +13,19 @@ class InspectorGUI : public olc::PixelGameEngine {
     std::unique_ptr<std::array<uint32_t,FRAMEBUFFER_LEN>> framebuffer_data;
     olc::QuickGUI::Manager toolbar;
     olc::QuickGUI::Button* refresh_button;
+
+    std::vector<std::string> source_code;    
+    olc::QuickGUI::TextBox* source_code_box;
+
     bool held_last = false;
+
+    std::string get_source_code_string() {
+        std::string ret;
+        for (auto& line : source_code) {
+            ret += line + "\n";
+        }
+        return ret;
+    }
 
 public:
 	InspectorGUI()
@@ -30,18 +43,17 @@ public:
         //TODO run configuration here
         bool ok = client.SendCommand();
         std::cout << "Server gave " << (ok ? "OK" : "not OK") << std::endl;
+        client.GetSourceCode(source_code);
 
         for (int i = 0; i < FRAMEBUFFER_LEN; i++)
             framebuffer_data->at(i) = 0;
 
         refresh_button = new olc::QuickGUI::Button(toolbar,"Refresh",olc::vf2d{0,vpu::defs::FRAMEBUFFER_HEIGHT},olc::vf2d{64.0f,TOOLBAR_HEIGHT});
         toolbar.AddControl(refresh_button);
-
-        std::vector<std::string> source_code;
-        client.GetSourceCode(source_code);
-        for (auto& line : source_code) {
-            std::cout << line << "\n";
-        }
+        source_code_box = new olc::QuickGUI::TextBox(toolbar, get_source_code_string(), olc::vf2d(300.0f, 8.0f), olc::vf2d(SOURCE_VIEW_WIDTH, 8*source_code.size()));
+        source_code_box->nAlign = olc::QuickGUI::Label::Alignment::Left;
+        source_code_box->bHasBorder = false;
+        toolbar.AddControl(source_code_box);
 
 		return true;
 	}
@@ -61,14 +73,14 @@ public:
 
         for (int y = 0; y < vpu::defs::FRAMEBUFFER_HEIGHT; y++) {
             for (int x = 0; x < vpu::defs::FRAMEBUFFER_WIDTH; x++) {
-                size_t ind = ScreenWidth()*y + x;
+                size_t ind = vpu::defs::FRAMEBUFFER_WIDTH*y + x;
                 uint32_t data = framebuffer_data->at(ind);
                 olc::Pixel colour = olc::Pixel(data);
                 Draw(x,y,colour);
             }
         }
 
-        toolbar.Draw(this);
+        toolbar.DrawDecal(this);
 
 		return true;
 	}
@@ -76,7 +88,7 @@ public:
 
 int main() {
 	InspectorGUI inspector;
-    if (inspector.Construct(vpu::defs::FRAMEBUFFER_WIDTH, vpu::defs::FRAMEBUFFER_HEIGHT + TOOLBAR_HEIGHT, 4, 4))
+    if (inspector.Construct(vpu::defs::FRAMEBUFFER_WIDTH + SOURCE_VIEW_WIDTH, vpu::defs::FRAMEBUFFER_HEIGHT + TOOLBAR_HEIGHT, 4, 4))
 		inspector.Start();
 
     return 0;
